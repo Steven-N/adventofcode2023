5cda69783c957ce10dcb706cd4cafc87242176dc
Steven Neville <stevenneville-dev@protonmail.com>
2024-11-28 22:14:38 -0500

    Setup advent of code 2024 repo

---
+++ day00/part1.py
@@ -0,0 +1,26 @@
+import argparse
+import pytest
+
+
+def compute(data):
+    pass
+
+INPUT = """
+"""
+
+@pytest.mark.parametrize(("test_input,expected"), [(INPUT, None)])
+def test(test_input, expected):
+    assert compute(test_input) == expected
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file")
+    args = parser.parse_args()
+
+    with open(args.file, "r") as f:
+        print(compute(f.read()))
+
+
+if __name__ == "__main__":
+    exit(main())
